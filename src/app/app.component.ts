354733adb84927d880684bffb24bee779e32e4c5
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-09-08 17:12:54 +0530

    if else condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  show = "yes";
+  buttonText = "False wali condition"
+
+  changeOnButton() {
+    this.show == 'yes' ? this.show = 'no' : this.show = 'yes';
+    this.show == 'no' ? this.buttonText = 'True wali condition' : this.buttonText = 'False wali condition';
+  }
 }
