c19501778efd00598958df2c79e9e53b7d1f5f82
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-09-09 12:59:22 +0530

    #18 For Loop | *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'title';
+  title = '#18 For Loop | *ngFor';
+
+  users = ['Preetam', 'Jitendra', 'Sushil', 'Deepak', 'Suresh'];
+  
+  usersDetail = [
+    {name:'Preetam', email:'preetam@gmail.com', mobile:'+91 4567544323', gender:'M'},
+    {name:'Jitendra', email:'jitendra@gmail.com', mobile:'+91 0987654321', gender:'M'},
+    {name:'Sushil', email:'sushil@gmail.com', mobile:'+91 2132435465', gender:'M'},
+    {name:'Nitika', email:'nitika@gmail.com', mobile:'+91 0989786756', gender:'F'},
+    {name:'Suresh', email:'suresh@gmail.com', mobile:'+91 0909898908', gender:'M'},
+  ];
 }
