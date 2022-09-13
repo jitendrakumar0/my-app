34aff59f1278f7c5c2b7e511aa8e35b353460ddb
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-09-13 15:16:56 +0530

    #26 Pass Data child to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'title';
+  indianRupees:number = 0;
+  inDoller:number = 0;
+
+  rendomValue(inputValue:any) {
+    this.indianRupees = inputValue;
+    // Math.floor(this.indianRupees * 79.18)
+    this.inDoller = Math.fround(this.indianRupees / 79.18);
+  }
 }
