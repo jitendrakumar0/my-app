a5d321f14301f0ad95ae81cf18e4a30c0a2c2cfd
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-09-14 13:14:15 +0530

    #28 Send data child to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'title';
+  title = '#28 Send data child to parent component';
 
   updateData(item:any) {
     console.warn(item);
