81faad7fbf1ed1946bfdb59da647373216087541
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-09-14 14:49:32 +0530

    #29 Two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'title';
+  title = '#29 Two way binding';
+
+  name:any;
 }
