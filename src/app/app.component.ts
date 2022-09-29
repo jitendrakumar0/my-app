e4ae309e5af39525f263d05d15b19f7927d138bc
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-09-29 18:41:32 +0530

    #37 Reactive form Validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'title';
+  title = '#37 Reactive form Validations';
+  submitBtn = false;
+
+  loginForm = new FormGroup({
+    username:new FormControl('', [Validators.required, Validators.email]),
+    password:new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
+  get user() {
+    return this.loginForm.get('username')
+  }
+  get password() {
+    return this.loginForm.get('password')
+  }
+
+  loginFormSubmit() {
+    console.info(this.loginForm.value);
+    if(this.loginForm.valid) {
+      this.submitBtn = true;
+      setTimeout(() => {
+        this.submitBtn = false;
+        this.loginForm.reset();
+      }, 2000);
+    }
+  }
+
 }
