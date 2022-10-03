c12e13d03cbd86a4ebdc16bfba18018a87d81db9
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-10-03 23:33:57 +0530

    jk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class AppComponent {
   title = '#37 Reactive form Validations';
-  submitBtn = false;
+  submitBtn:boolean = false;
+  disabledOnSubmit:boolean = true;
 
   loginForm = new FormGroup({
     username:new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
@@ -26,9 +27,15 @@ export class AppComponent {
     console.info(this.loginForm.value);
     if(this.loginForm.valid) {
       this.submitBtn = true;
+      this.loginForm.controls['username']?.disable();
+      this.loginForm.controls['password']?.disable();
+      this.disabledOnSubmit = false;
       setTimeout(() => {
         this.submitBtn = false;
         this.loginForm.reset();
+        this.loginForm.controls['username']?.enable();
+        this.loginForm.controls['password']?.enable();
+        this.disabledOnSubmit = true;
       }, 2000);
     }
   }
