aa405acf510bf031a5a0a6b786da0d50abbe69e4
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-10-03 23:57:23 +0530

    #42 Child Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { NoPageComponent } from './no-page/no-page.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
@@ -8,7 +10,14 @@ import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
   {path: "" , component:HomeComponent},
-  {path: "about" , component:AboutComponent},
+  {
+    path: "about",
+    component:AboutComponent,
+    children:[
+      {path:"me" , component:AboutMeComponent},
+      {path:"company" , component:AboutCompanyComponent},
+    ]
+  },
   {path: "contact" , component:ContactComponent},
   {path: "user/:id" , component:UserComponent},
   {path: "**" , component:NoPageComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { UserComponent } from './pages/user/user.component';
 import { NoPageComponent } from './no-page/no-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NoPageComponent } from './no-page/no-page.component';
     AboutComponent,
     ContactComponent,
     UserComponent,
-    NoPageComponent
+    NoPageComponent,
+    AboutMeComponent,
+    AboutCompanyComponent
   ],
   imports: [
     BrowserModule,
