6815bcbfd80239fe6ef8f275ef7869efb8c3774d
jitendraeorchids <98940400+jitendraeorchids@users.noreply.github.com>
2022-10-06 19:43:52 +0530

    # Model and Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+interface usersType {
+  id:number,
+  name:string,
+  indian:boolean,
+  address:any
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'title';
+  title = '# Model and Interface';
+
+  users:usersType[] = [
+    {
+      id:1,
+      name:"Jitendra Kumar Bijarnia",
+      indian:true,
+      address:"54 Street, USA"
+    }
+  ]
 }
